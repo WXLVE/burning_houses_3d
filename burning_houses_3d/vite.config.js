485cf72afbe9956003b44607cd34e9d420015828
WXLVE <daniel.smit.nl@gmail.com>
2026-03-24 14:14:22 +0100

    small change to base to reflect the underscore in the folder

--- burning_houses_3d/vite.config.js
+++ burning_houses_3d/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/burning-houses-3d/',
+  base: '/burning_houses_3d/',
 })
